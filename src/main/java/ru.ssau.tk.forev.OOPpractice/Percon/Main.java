ecc6075fc5cc7707192276993e33badbe3211e80
Agapova <agapovadaria2019@gmail.com>
2019-09-15 03:20:00 +0400

    Задание 1.1

---
+++ src/main/java/ru.ssau.tk.forev.OOPpractice/Percon/Main.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.forev.OOPpractice.Percon;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person onePeople = new Person("Kate", "Petrova", 6315);
+
+        System.out.println(" Имя — " + onePeople.getFirstName());
+        System.out.println(" Фамилия — " + onePeople.getLastName());
+        System.out.println("Номер паспорта — " + onePeople.getPassportId());
+
+        Person twoPeople = new Person("Victor", "Long", 8546);
+
+        System.out.println(" Имя — " + twoPeople.getFirstName());
+        System.out.println(" Фамилия — " + twoPeople.getLastName());
+        System.out.println("Номер паспорта — " + twoPeople.getPassportId());
+
+        Person threePeople = new Person("Lada", "Bukina", 1469);
+
+        System.out.println(" Имя — " + threePeople.getFirstName());
+        System.out.println(" Фамилия — " + threePeople.getLastName());
+        System.out.println("Номер паспорта — " + threePeople.getPassportId());
+    }
+
+}
+
---
+++ src/main/java/ru.ssau.tk.forev.OOPpractice/Percon/Person.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.forev.OOPpractice.Percon;
+
+class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+
+    Person(String firstName, String lastName, int passportId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+    }
+
+    String getFirstName() {     //возвращает значение поля  firstName  у того объекта, для которого он был вызван.
+
+        return firstName;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    int getPassportId() {
+        return passportId;
+    }
+
+    void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+}
