f57f351b5230fefa4b0aeedc35b6d832fae7983a
Agapova <agapovadaria2019@gmail.com>
2019-09-22 23:40:24 +0400

    Задание 1.3

---
+++ src/main/java/ru.ssau.tk.forev.OOPpractice/Points/Main.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.forev.OOPpractice.Points;
+
+public class Main {
+    public static void main(String[] args) {
+        Point one = new Point(1, 2, 5);
+        Point two = new Point(-100,20,60);
+    }
+}
