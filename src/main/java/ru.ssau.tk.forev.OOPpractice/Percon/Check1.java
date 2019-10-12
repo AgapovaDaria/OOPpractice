b08a747ad3e9fd2400b89960828f18a6a446a128
Agapova <agapovadaria2019@gmail.com>
2019-10-13 02:25:27 +0400

    Task 1.14 (+explanation of output)

---
+++ src/main/java/ru.ssau.tk.forev.OOPpractice/Percon/Check1.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.forev.OOPpractice.Percon;
+
+public class Check1 {
+    /*
+    Сначала на консоли выводится Ignat, т.к. копия ссылки на объект переопределяется методом checkAnotherPerson()
+    новым объектом person, причем только во время работы метода.Потом выводится Arkadiy, т.к.
+    идет обращение к исходному объекту.
+     */
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setFirstName("Arkadiy");
+        checkAnotherPerson(person);
+        System.out.println(person.getFirstName());
+    }
+
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
+        System.out.println(person.getFirstName());
+    }
+}
