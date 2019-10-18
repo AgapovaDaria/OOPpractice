d84bf2713d979fd88f543dff4295ad1af3c93b74
Agapova <agapovadaria2019@gmail.com>
2019-10-18 14:28:51 +0400

    Task 1.20

---
+++ src/main/java/ru.ssau.tk.forev.OOPpractice/ClassWrappers.java
@@ -0,0 +1,140 @@
+package ru.ssau.tk.forev.OOPpractice;
+
+public class ClassWrappers {
+
+    @Deprecated
+    static Integer boxing(int i) {
+        return new Integer(i);
+    }
+
+    @Deprecated
+    static Boolean boxing(boolean b) {
+        return new Boolean(b);
+    }
+
+    @Deprecated
+    static Short boxing (short s) {
+        return new Short(s);
+    }
+
+    @Deprecated
+    static Double boxing (double d) {
+        return new Double(d);
+    }
+
+    @Deprecated
+    static Float boxing (float f) {
+        return new Float(f);
+    }
+
+    @Deprecated
+    static Long boxing (long l){
+        return new Long(l);
+    }
+
+    @Deprecated
+    static Character boxing (char c) {
+        return new Character(c);
+    }
+
+    @Deprecated
+    static Byte boxing (byte byt) {
+        return new Byte(byt);
+    }
+
+    static int unboxing(Integer i) {
+        return i.intValue();
+    }
+
+    static boolean unboxing(Boolean b) {
+        return b.booleanValue();
+    }
+
+    static short unboxing(Short s) {
+        return s.shortValue();
+    }
+
+    static double unboxing(Double d) {
+        return d.doubleValue();
+    }
+
+    static float unboxing(Float f) {
+        return f.floatValue();
+    }
+
+    static long unboxing(Long l) {
+        return l.longValue();
+    }
+
+    static char unboxing(Character c) {
+        return c.charValue();
+    }
+
+    static byte unboxing(Byte byt){
+        return byt.byteValue();
+    }
+
+    static Integer autoBoxing(int p) {
+        return p;
+    }
+
+    static Boolean autoBoxing(boolean p) {
+        return p;
+    }
+
+    static Short autoBoxing(short p) {
+        return p;
+    }
+
+    static Double autoBoxing(double p) {
+        return p;
+    }
+
+    static Float autoBoxing(float p) {
+        return p;
+    }
+
+    static Long autoBoxing(long p) {
+        return p;
+    }
+
+    static Character autoBoxing(char p) {
+        return p;
+    }
+
+    static Byte autoBoxing(byte p) {
+        return p;
+    }
+
+    static int autoUnboxing(Integer i) {
+        return i;
+    }
+
+    static boolean autoUnboxing(Boolean b) {
+        return b;
+    }
+
+    static short autoUnboxing(Short s) {
+        return s;
+    }
+
+    static double autoUnboxing(Double d) {
+        return d;
+    }
+
+    static float autoUnboxing(Float f) {
+        return f;
+    }
+
+    static long autoUnboxing(Long l) {
+        return l;
+    }
+
+    static char autoUnboxing(Character c) {
+        return c;
+    }
+
+    static byte autoUnboxing(Byte byt){
+        return byt;
+    }
+}
