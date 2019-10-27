4a7ca5b0f7d0410fddc9657882e4cc33ff0dea24
Agapova <agapovadaria2019@gmail.com>
2019-10-28 02:54:10 +0400

    Task 2.7 (+Test)

--- src/main/java/ru.ssau.tk.forev.OOPpractice/Array/Array.java
+++ src/main/java/ru.ssau.tk.forev.OOPpractice/Array/Array.java
@@ -1,10 +1,10 @@
 package ru.ssau.tk.forev.OOPpractice.Array;
 
 public class Array {
-    static public double [] newArray(int length) {
+    static public double[] newArray(int length) {
         double[] arrayDimension = new double[length];
         for (int i = 0; i < length; i++) {
-            arrayDimension[i] = i+1;
+            arrayDimension[i] = i + 1;
         }
         return arrayDimension;
     }
@@ -12,7 +12,7 @@ public class Array {
     static public double[] newArrayTwo(int length) {
         double[] arrayDimension = new double[length];
         for (int i = 1; i < length - 1; i++) {
-           arrayDimension[i] = 1;
+            arrayDimension[i] = 1;
         }
         arrayDimension[0] = 2;
         arrayDimension[length - 1] = 2;
@@ -30,7 +30,7 @@ public class Array {
     static public double[] newArrayFour(int length) {
         double[] arrayDimension = new double[length];
         for (int i = 0; i < length; i++) {
-            arrayDimension[i] = 2*length - 2*i;
+            arrayDimension[i] = 2 * length - 2 * i;
         }
         return arrayDimension;
     }
@@ -48,8 +48,31 @@ public class Array {
     static public double[] newArraySix(int length) {
         double[] arrayDimension = new double[length];
         for (int i = 0; i < length; i++) {
-            arrayDimension[i] = i*i;
+            arrayDimension[i] = i * i;
         }
         return arrayDimension;
     }
+
+    static double[] quadraticEquationSolver(double a, double b, double c) {
+        if (a == 0) {
+            if (b == 0) {
+                return new double[0];
+            } else {
+                return new double[]{-c / b};
+            }
+        }
+        double D = b * b - 4 * a * c;
+        if (D > 0) {
+            double x1=(-b + Math.sqrt(D)) / (2 * a);
+            double x2=(-b - Math.sqrt(D)) / (2 * a);
+            return new double[]{x1,x2};
+        } else if (D == 0) {
+            double x0=(-b + Math.sqrt(D)) / (2 * a);
+            return new double[]{x0};
+        } else {
+            return new double[0];
+        }
+    }
+
 }
+
--- src/test/java/ru/ssau/tk/forev/OOPpractice/Array/ArrayTest.java
+++ src/test/java/ru/ssau/tk/forev/OOPpractice/Array/ArrayTest.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.forev.OOPpractice.Array;
 
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class ArrayTest {
@@ -19,17 +20,17 @@ public class ArrayTest {
             assertEquals(array[i], 1, 0.000001);
         }
         assertEquals(array[0], 2, 0.000001);
-        assertEquals(array[length - 1], 2,0.000001);
+        assertEquals(array[length - 1], 2, 0.000001);
     }
 
     @Test
     public void testArrayThree() {
         int length = 20;
         double[] arrayDimension = Array.newArrayThree(length);
-        for (int i = 0; i < length ; i++) {
-            assertEquals(arrayDimension [i],2 * i + 1 , 0.000001);
+        for (int i = 0; i < length; i++) {
+            assertEquals(arrayDimension[i], 2 * i + 1, 0.000001);
         }
-        assertEquals(arrayDimension [1],3 , 0.000001);
+        assertEquals(arrayDimension[1], 3, 0.000001);
     }
 
     @Test
@@ -37,9 +38,9 @@ public class ArrayTest {
         int length = 10;
         double[] arrayDimension = Array.newArrayFour(length);
         for (int i = 0; i < length; i++) {
-            assertEquals(arrayDimension [i], 2*length - 2*i, 0.000001);
+            assertEquals(arrayDimension[i], 2 * length - 2 * i, 0.000001);
         }
-        assertEquals(arrayDimension [9],2 , 0.000001);
+        assertEquals(arrayDimension[9], 2, 0.000001);
     }
 
     @Test
@@ -47,9 +48,9 @@ public class ArrayTest {
         int length = 6;
         double[] arrayDimension = Array.newArrayFive(length);
         for (int i = 2; i < length; i++) {
-            assertEquals(arrayDimension [i], arrayDimension[i - 1] + arrayDimension[i - 2], 0.000001);
+            assertEquals(arrayDimension[i], arrayDimension[i - 1] + arrayDimension[i - 2], 0.000001);
         }
-        assertEquals(arrayDimension [4],3 , 0.000001);
+        assertEquals(arrayDimension[4], 3, 0.000001);
     }
 
     @Test
@@ -57,8 +58,22 @@ public class ArrayTest {
         int length = 6;
         double[] arrayDimension = Array.newArraySix(length);
         for (int i = 0; i < length; i++) {
-            assertEquals(arrayDimension [i], i*i, 0.000001);
+            assertEquals(arrayDimension[i], i * i, 0.000001);
         }
-        assertEquals(arrayDimension [1],1 , 0.000001);
+        assertEquals(arrayDimension[1], 1, 0.000001);
+    }
+
+    @Test
+    public void testQuadraticEquationSolver() {
+        double[] rstExample = Array.quadraticEquationSolver(0, 0, 50);
+        assertEquals(rstExample, new double[0]);
+        double[] secondExample = Array.quadraticEquationSolver(0, 10, -5);
+        assertEquals(secondExample[0], 0.5, 0.00001);
+        double[] thirdExample = Array.quadraticEquationSolver(2, -4, 0);
+        assertEquals(thirdExample[1], 0./2, 0.00001);
+        double[] fourExample = Array.quadraticEquationSolver(4, -4, 1);
+        assertEquals(fourExample[0], 0.5, 0.00001);
+        double[] fiveExample = Array.quadraticEquationSolver(1, 2, 5);
+        assertEquals(fiveExample, new double[0]);
     }
 }
